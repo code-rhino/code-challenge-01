5f63f355a183f2f362f67466f95fe8866b9a79d4
Alvina Waseem <alvinawaseem@Alvinas-Air.fios-router.home>
2021-11-08 11:10:10 -0500

    not done

--- src/main/java/com/codedifferently/countdups/Program01.java
+++ src/main/java/com/codedifferently/countdups/Program01.java
@@ -1,8 +1,23 @@
 package com.codedifferently.countdups;
 
+import java.util.Arrays;
+
 public class Program01 {
 
-    public String countUniqueWords(String input){
-        return null;
+    public static String countUniqueWords(String input){
+        String words[] = input.split(" ");
+        int count = 0;
+
+        for(int i=0; i < words.length; i++){
+            if(words[0].equals(words[i])){
+                count++;
+            }
+        }
+
+        return Arrays.toString(words) + "\n First Word " +  count+ "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countUniqueWords("if else if if for"));
     }
 }
