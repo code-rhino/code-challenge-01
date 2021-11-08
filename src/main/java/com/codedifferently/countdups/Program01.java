1fee9ff78a20b088775cae2e82356fbcae6eaafc
Tajah GARVIN <tajahgarvin@Tajahs-MacBook-Air-2.local>
2021-11-08 11:10:21 -0500

    tried

--- src/main/java/com/codedifferently/countdups/Program01.java
+++ src/main/java/com/codedifferently/countdups/Program01.java
@@ -1,8 +1,22 @@
 package com.codedifferently.countdups;
 
-public class Program01 {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
-    public String countUniqueWords(String input){
-        return null;
+public class Program01  {
+
+
+       public String countUniqueWords (String input){
+
+
+            String[] strArray = input.split(" ");
+            List<String> repeatedWords = new ArrayList<>();
+            HashSet<String> uniqueWords = new HashSet<>();
+            for (String str : strArray) {
+                if (!uniqueWords.add(str))
+                    repeatedWords.add(str);
+            }
+            return repeatedWords.toString();
+        }
     }
-}
