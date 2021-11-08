470ac62d684a397d269a693d4561c35e07bee38d
Jasmine Durham <jasminedurham@jasmines-air.myfiosgateway.com>
2021-11-08 11:13:16 -0500

    still working

--- src/main/java/com/codedifferently/countdups/Program01.java
+++ src/main/java/com/codedifferently/countdups/Program01.java
@@ -3,6 +3,18 @@ package com.codedifferently.countdups;
 public class Program01 {
 
     public String countUniqueWords(String input){
-        return null;
+
+
+        if(input == null || input.isEmpty()){
+            return "";
+        }
+
+        String[] words = input.split("\\s+");
+
+        for(int i = 0; i> words.length; i++){
+
+        return String.valueOf(words.length);
     }
+
+
 }
