0ff71f83812334bf5c6bd250ae96d57f27839170
yborelandcdmac <yboreland19@archmerealumni.org>
2021-11-08 11:11:53 -0500

    whatever this is

--- src/main/java/com/codedifferently/countdups/Program01.java
+++ src/main/java/com/codedifferently/countdups/Program01.java
@@ -1,8 +1,22 @@
 package com.codedifferently.countdups;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program01 {
 
     public String countUniqueWords(String input){
+        String a[] = input.split(" ");
+        List<String> duplicates = new ArrayList<>();
+
+
+        int count = 0;
+        for (int i = 0; i < input.length(); i++ ){
+            if
+
+        }
+
+
         return null;
     }
 }
